4892912d39cf1d6ac8844c9077984e35d06535d7
Carl Hayes <HeyItskPan1c@gmail.com>
2019-01-16 09:20:39 -0800

    removed Comments

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.backgroundView.backgroundColor = UIColor.gray()
+        
     }
 
 
